2d787489faa623eed0bc3bd8b2227fd258033c14
Koima-Sam <sam17koima@gmail.com>
2022-07-01 18:16:33 +0300

    commit tree 669a8f2cbbd48b5683d9708b53ea1b6477223a00

--- index.js
+++ index.js
@@ -8,6 +8,77 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+// Your code here
+function createEmployeeRecord(array){
+    return{
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents : [],
+        timeOutEvents: []
+    }
+}
+
+
+function createEmployeeRecords(arrayRecords){
+    return arrayRecords.map(records => createEmployeeRecord(records));
+}
+
+function createTimeInEvent(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+
+function createTimeOutEvent(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+
+function hoursWorkedOnDate(dateStamp){
+    let hoursWorked
+    let hourIn
+    let hourOut
+    this.timeInEvents.forEach((timeIn)=>{
+        if (timeIn.date === dateStamp){
+            hourIn = timeIn.hour/100
+        }
+    })
+    this.timeOutEvents.forEach((timeOut)=>{
+        if (timeOut.date === dateStamp){
+            hourOut = timeOut.hour/100
+        }
+    })
+    hoursWorked = hourOut - hourIn
+    return hoursWorked
+}
+function wagesEarnedOnDate(dateStamp){
+    return hoursWorkedOnDate(this,dateStamp)* this.payPerHour;
+}
+
+
+function calculatePayroll(employeeRecordArray){
+    let totalPayOwed = 0
+    employeeRecordArray.forEach(employeeRecord => {
+       totalPayOwed += allWagesFor(employeeRecord)
+    })
+    return totalPayOwed
+}
+
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
